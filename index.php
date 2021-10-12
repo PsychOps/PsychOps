329f1b11cca094a7985348f812b406e511704c00
joshua <joshuaslui0203@gmail.com>
2021-10-12 22:55:55 +0200

    I hate css.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/html/head.php' ;?>
+
+<?php include 'templates/html/navigation.php';?>
+<body>
+
+<container class="container">
+<!--
+    <div style="padding-bottom: 20px">
+        <h1 class="notnavtext position-absolute bottom-50 start-50 translate-middle fs-1">About Modraxis</h1>
+    </div>
+-->
+    <div style="padding-top: 20px">
+        <p class="notnavtext position-absolute top-50 start-50 translate-middle text-center" style="overflow: auto">
+        Modraxis is a Netherlands based organization owned by <a href="https://zoniq.tk" target="_blank">Joshua</a> and <a href="#">Jay</a> dedicated to open-source software and web development with <b>3</b> developers, and an additional <b>3</b> staff members. With user experience and feedback in mind, Modraxis Development always attempts to provide its users with products that are to their liking.
+        Additionally, Modraxis also functions as a hub for developers of any field. With our main discord server, we allow getting support for any coding language and our products, from both the community as well as the staff team at Modraxis.
+        </p>
+    </div>
+</container>
+
+</body>
+
+</html>
---
+++ templates/html/head.php
@@ -0,0 +1,16 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>    <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="templates/css/navigation.css">
+    <link rel="stylesheet" href="templates/css/formatting.css">
+    <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <meta charset="utf-8">
+
+    <title>Modraxis Development</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
