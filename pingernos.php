81492555a9b8d32e9dfa310aeae09c3bfd77dc08
joshua <joshuaslui0203@gmail.com>
2021-10-22 15:26:52 +0200

    add btn-rounded

--- pingernos.php
+++ pingernos.php
@@ -17,7 +17,7 @@
     <img src="assets/Pingernos.png" alt="" class="pingernos-img">
     <br>
     <br>
-    <a href="https://discordextremelist.xyz/en-US/bots/889197952994791434" target="_blank" class="btn bg-green">Pingernos website</a>
+    <a href="https://discordextremelist.xyz/en-US/bots/889197952994791434" target="_blank" class="btn bg-green btn-rounded">Pingernos website</a>
 </div>
 </body>
 
