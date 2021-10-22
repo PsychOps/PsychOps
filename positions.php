65cea6f4273a251fd181474bf77d68d7693e303f
joshua <joshuaslui0203@gmail.com>
2021-10-22 14:41:33 +0200

    Add positions page

---
+++ positions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/html/head.html' ;?>
+
+<?php include 'templates/html/navigation.php' ;?>
+
+<body>
+
+<div class="notnavtext text-center">
+    <img src="/assets/PsychOpsTransparent.png" alt="">
+    <p class="fs-1">Positions</p>
+    <p>
+    Hello! Thank you for your interest, but we do not have any positions open at the moment.
+    </p>
+    <a href="https://discord.gg/QMhvHuGPMx" class="btn bg-green btn-rounded">Join our discord to know when positions open</a>
+</div>
+</body>
+
+</html>
