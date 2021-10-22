354b3e39b8269700a612bd0bbd897eda63908533
joshua <joshuaslui0203@gmail.com>
2021-10-22 14:41:24 +0200

    Add positions to sidenav

--- templates/html/navigation.php
+++ templates/html/navigation.php
@@ -20,7 +20,7 @@ var collapseList = collapseElementList.map(function (collapseEl) {
         <a href="team.php" class="list-group-item list-group-item-action py-2 ripple bg-button">
           <i class="fas fa-users fa-fw me-3"></i><span>Our team</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple bg-button"
+        <a href="positions.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
           ><i class="fas fa-laptop-code fa-fw me-3"></i><span>Open positions</span></a
         >
         <a href="#" class="list-group-item list-group-item-action py-2 ripple bg-button"
