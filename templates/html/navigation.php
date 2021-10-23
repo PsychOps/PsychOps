6de033e528646264194013562384512bd22ef636
joshua <joshuaslui0203@gmail.com>
2021-10-23 13:56:59 +0200

    Add mg

--- templates/html/navigation.php
+++ templates/html/navigation.php
@@ -26,7 +26,7 @@ var collapseList = collapseElementList.map(function (collapseEl) {
         <a href="commissions.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
           ><i class="fas fa-coins fa-fw me-3"></i><span>Commissions</span></a
         >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple bg-button">
+        <a href="mg.php" class="list-group-item list-group-item-action py-2 ripple bg-button">
           <i class="fas fa-cubes fa-fw me-3"></i><span>MinecraftGolden</span>
         </a>
         <a href="pingernos.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
