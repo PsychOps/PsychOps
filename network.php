ce8513573329c1e160e2dcfa07e0a9374a058ccf
joshua <zoniqhedgehog@gmail.com>
2021-11-10 16:52:25 +0100

    Add network page

---
+++ network.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/html/head.html' ;?>
+
+<?php include 'templates/html/navigation.php' ;?>
+
+<body>
+
+<div class="notnavtext text-center">
+    <p class="fs-1">Network</p>
+    <p>
+    Welcome to our network page. Here we feature the network of discord servers, affiliates and subsidiaries we control. All of these are currently managed or owned by one or more team members from PsychOps. Want to join in our network? Join our discord server and shoot a DM towards Jay or Faze or <a href="mailto:psychopsorg@gmail.com">mail to us</a>.
+    </p>
+    <a href="https://discord.gg/QMhvHuGPMx" class="btn bg-green btn-rounded">Central discord server</a>
+</div>
+
+<div class="notnavtext text-center">
+  <div class="cards">
+      <div class="card">
+          <div class="card-content">
+              <div class="imgBx">
+                  <a href="https://discord.gg/gPZNGANWCE" target="_blank"><img src="assets/network/BadWolf.png" alt=""></a>
+              </div>
+              <div class="contentBx">
+                  <h4>Bad Wolf</h4>
+                  <h5>Community</h5>
+                  <p>Our main purpose and goal is to give people a freedom-filled discord server, with many activities to silence the feeling of boredom with little rules - yet enough to not end in total anarchy.</p>
+              </div>
+          </div>
+      </div>
+      <div class="card">
+          <div class="card-content">
+              <div class="imgBx">
+                  <a href="mg.php" target="_blank"><img src="assets/MG/MG.png" alt=""></a>
+              </div>
+              <div class="contentBx">
+                  <h4>MinecraftGolden</h4>
+                  <h5>Minecraft Server</h5>
+                  <p>MinecraftGolden is a minecraft server hosted through Aternos. Owned by Bench182 and managed by PsychOps as part of our community program, offering both survival and duels.</p>
+              </div>
+          </div>
+      </div>
+      <div class="card">
+        <div class="card-content">
+            <div class="imgBx">
+                <img src="assets/network/TheCabin.png" alt="">
+            </div>
+            <div class="contentBx">
+                <h4>The Cabin</h4>
+                <h5>LGBTQ+ Advise & community</h5>
+                <p>Hi! We are a safe haven for members of the LGBTQ+ community, offering advice, and a lot more to anyone that finds us!</p>
+            </div>
+        </div>
+    </div>
+      <div class="card">
+          <div class="card-content">
+              <div class="imgBx">
+                  <a href="https://discord.gg/u6FKYxKGBc" target="_blank"><img src="assets/network/Haven.webp" alt=""></a>
+              </div>
+              <div class="contentBx">
+                  <h4>The Haven</h4>
+                  <h5>Community</h5>
+                  <p>Currently no information available about this affiliate. Try joining their discord server to know more about them.</p>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+
+</body>
+
+</html>
