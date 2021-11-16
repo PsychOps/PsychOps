89066b28421590b4606e84b1431bffc0f5d73e06
joshua <zoniqhedgehog@gmail.com>
2021-11-16 23:21:46 +0100

    Update legal privacy policy

--- legal.php
+++ legal.php
@@ -18,8 +18,27 @@
     Our ("PsychOps Org.") bots ("Pingernos", "Healver", "PsychOps") do not gather any data from it's users.
     <br/>Similarly, our website ("PsychOps") does not use cookies, and does not gather data.
     </p>
+    <span class="fs-3">BadWolf Bot</span>
     <p>
-    We ("PsychOps Org.") and the PsychOps Org. owners ("Joshua Slui" and "Jason Musser") do not hold accountability nor liability for any issues found with our products when not sourced by PsychOps Org.
+    BadWolf bot (Belonging to "Bad Wolf Org.") gathers data to function in a proper manner. This data can be removed per request, but it may take up to 7 days for all your data to be gathered and deleted by our team. Any of the gathered data is only accessible to the BadWolf owners - Etile and Joshua.
+    <br/>
+    <br/>
+    </p>
+    <span class="fs-3">BadWolf data collection</span>
+    <p>
+    We collect Guild IDs (guild prefixes, guild blacklist*) and User IDs (suggestions, user blacklists*). We need to store this data for guild prefixes and guild blacklists to remain guild specific. User IDs are collected for suggestions so that we can inform a member about their suggestion status without them being in our support server. <b>Your data is not provided, sold or shared to any 3d party vendors or parties.</b>
+    <br>
+    * These are only gathered if a guild or user gets blacklisted on our bot, together with the respective reason.
+    </p>
+</div>
+<div class="notnavtext text-center" id="liability">
+    <hr>
+        <a href=#liability class="txt-white">
+        <i class="fas fa-link fa-2x"></i>
+        <span class="fs-1">Liability</span>
+        </a>
+    <p>
+    We ("PsychOps Org.", "BadWolf Org.") and the PsychOps Org. owners ("Joshua Slui" and "Jason Musser") as well as the BadWolf Org. owners ("Joshua Slui", "Etile") do not hold accountability nor liability for any issues found with our products when not sourced by PsychOps Org.
     </p>
     <p>
     For any concerns or questions, please contact us below.
