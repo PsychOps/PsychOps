379cc2e13ef619724a57bf4d52715bea9ace2600
joshua <zoniqhedgehog@gmail.com>
2021-11-27 12:17:40 +0100

    Add user rights, data processor etc

--- legal.php
+++ legal.php
@@ -9,7 +9,7 @@
 
 <div class="notnavtext" id="section1">
     <h1 class="fs-1 text-center">Privacy Policy</h1>
-    <p style="font-weight: bold" class="sectionstart">Last updated November 24 2021</p>
+    <p style="font-weight: bold" class="sectionstart">Last updated November 27 2021</p>
 
 <div class="content">
     <h1 class="fs-4">Table Of Contents</h1>
@@ -20,6 +20,7 @@
     <li> <a href=#children>Children under the age of 16</a> </li>
     <li> <a href=#changes>Privacy Policy changes</a> </li>
     <li> <a href=#removal>Data removal requests</a> </li>
+    <li> <a href=#rights>User Rights</a> </li>
     <li> <a href=#contact>Contact Us</a> </li>
     </ul>
 </div>
@@ -29,6 +30,8 @@
         PsychOps' mission is to deliver completely free and open source products to anyone as alternative to paid software, or as entertainment product. To ensure that our mission succeeds, we have to gather certain data from our users for our products. While we gather data, we do wish to be as transparent and respectful about it as we can.
         <br/> <br/>
         This privacy policy only applies when you use our Services. Please read this Privacy Policy before engaging with our Services as this will tell you how we collect, store, use, disclose, and otherwise process, your data when providing these Services to you. <b>Please note that this privacy policy is NOT a contract and does not create any obligations or legal rights furthermore.</b>
+        <br/> <br/>
+        The PsychOps owner ("Joshua Slui") is legally responsible for and in charge of the services processing the user data (With exception of Google Analytics). For any concern and/or questions you can send a mail to <a href="mailto:joshuaslui0203@gmail.com">joshuaslui0203@gmail.com</a>
         </p>
     </div>
 </div>
@@ -39,7 +42,7 @@
         <p>
         PsychOps develops and maintains public, open-source websites and Discord bots at no cost. "PsychOps", "BadWolf", "we", "our" or "us" in this privacy policy refers to PsychOps Org. as well as any of its subsidiaries.
         <br/>
-        PsychOps is a software development organisation situated in The Netherlands. PsychOps follows the laws laid onto companies by the European Union and will not under any circumstances follow other laws despite being globally available. Users situated outside of the EU are by exception still allowed to invoke European rights in relation to data deletion or any other legal circumstances.
+        PsychOps is a software development organization situated in The Netherlands. PsychOps follows the laws laid onto companies by the European Union and will not under any circumstances follow other laws despite being globally available. Users situated outside of the EU are by exception still allowed to invoke European rights in relation to data deletion or any other legal circumstances.
     </div>
 </div>
 
@@ -75,7 +78,7 @@
 <div class="notnavtext textend" id="how">
     <div class="sectionstart">
         <h1 class="fs-2">How we collect data</h1>
-        <p>The data collected by Google Analytics within the PsychOps website is collected as soon as the user enters the website, and is sent to the Google Analytics panel that is accessible only to the PsychOps owner ("Joshua Slui"). Meanwhile, the BadWolf bot collects data as soon as the appropiate commands are executed. These commands are listed below:</p>
+        <p>The data collected by Google Analytics within the PsychOps website is collected as soon as the user enters the website, and is sent to the Google Analytics panel that is accessible only to the PsychOps owner ("Joshua Slui"). Meanwhile, the BadWolf bot collects data as soon as the appropriate commands are executed. These commands are listed below:</p>
         <ul>
             <li>suggest</li>
             <li>prefix set</li>
@@ -89,7 +92,7 @@
 <div class="notnavtext textend" id="children">
     <div class="sectionstart">
         <h1 class="fs-2">Children under the age of 16</h1>
-        <p>According to <a href="https://gdpr-info.eu/art-8-gdpr/">GDPR Article 8</a>, it is not legally allowed to gather data of users under 16 without their, or their guardian's explicit consent. We do not knowingly gather data of people below 16, and our website is not dedicated to anyone under that age. If you are below this age, please consider clearing your cookies, cache and closing this tab.</p>
+        <p>According to <a href="https://gdpr-info.eu/art-8-gdpr/">GDPR Article 8</a>, it is not legally allowed to gather data of users under 16 without their guardian's explicit consent. We do not knowingly gather data of people below 16, and our website is not dedicated to anyone under that age. If you are below this age, please consider clearing your cookies, cache and closing this tab.</p>
     </div>
 </div>
 
@@ -104,6 +107,17 @@
     <div class="sectionstart">
     <h1 class="fs-2">Data removal requests</h1>
     <p>It is possible to get your data on BadWolf removed on request. For that, please <a href=#contact>contact us</a> and submit the data you want removed, additionally with the reason. Of course, we require your user ID or guild ID as well, depending on what data you want removed. <b>Removing this data may take up to 3 days, please be patient and wait for a team member to get to you upon sending in your request.</b>
+    </div>
+</div>
+
+<div class="notnavtext textend" id="rights">
+    <div class="sectionstart">
+        <h1 class="fs-2">User Rights</h1>
+        <p>We save all mentioned data starting from when you start using our products (including this website). You have the right to information about your personal data as well as for correction. Additionally, European users have access to something called "<a href="https://en.wikipedia.org/wiki/Right_to_be_forgotten" target="_blank">the right to be forgotten</a>". We will always accept any and all requests for data deletion or data changes unless its in whereabouts to our blacklist(s).
+        <br/> <br/>
+        Furthermore, according to <a href="https://gdpr-info.eu/art-8-gdpr/">GDPR</a>, you have the right to lodge a complaint with a supervisory authority if you believe that the processing of your personal data is illicit.
+        <br/> <br/>
+        You are only allowed to agree to this policy if you are at least 16 years old (or less if your country has different regulations) or if consent is given by the holder of your parental responsibility. You can read the <a href=#children>"Children under the age of 16"</a> section for more info.</p>
     </div>
 </div>
 
