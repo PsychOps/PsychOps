4de5fe9de1d8594f5f6111a6f71540571015a02c
Hilex23 <89878821+Hilex23@users.noreply.github.com>
2021-11-28 15:18:46 +0100

    Various changes to the website
    Created Projects page, created Varumox page and edited the navbar

--- templates/html/navigation.php
+++ templates/html/navigation.php
@@ -1,112 +1,117 @@
+<link rel="shortcut icon" href="/assets/S1.png" type="image/x-icon" />
+  </head>
 </script>
-    <!--Main Navigation-->
-    <header>
-      <!-- Sidebar -->
-      <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-dark">
-        <div class="position-sticky">
-          <div class="list-group list-group-flush mx-3 mt-4">
-            <a
-              href="index.php"
-              class="list-group-item list-group-item-action py-2 ripple bg-button"
-              aria-current="true"
-            >
-              <i class="fas fa-address-card fa-fw me-3"></i><span>About us</span></a
-              >
-            <a href="team.php" class="list-group-item list-group-item-action py-2 ripple bg-button">
-              <i class="fas fa-users fa-fw me-3"></i><span>Our team</span></a
-              >
-            <a href="positions.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
-              ><i class="fas fa-laptop-code fa-fw me-3"></i><span>Open positions</span></a
-            >
-            <a href="associates.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
-              ><i class="fas fa-handshake fa-fw me-3"></i><span>Associates</span></a
-            >
-            <a href="commissions.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
-              ><i class="fas fa-coins fa-fw me-3"></i><span>Commissions</span></a
-            >
-            <a href="#" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item-head"
-              ><i class="fas fa-network-wired fa-fw me-3"></i><span>Projects</span><i class="fas fa-sort-down"></i></a
-            >
-            <a href="pingernos.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item"
-              ><i class="fas fa-server fa-fw me-3"></i><span>Pingernos</span></a
-            >
-            <a href="network.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item-head"
-              ><i class="fas fa-network-wired fa-fw me-3"></i><span>Network</span><i class="fas fa-sort-down"></i></a
-            >
-            <a href="badwolf.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item">
-                <i class="fas fa-paw fa-fw me-3"></i><span>Bad Wolf</span></a
-            >
-            <a href="mg.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item">
-              <i class="fas fa-cubes fa-fw me-3"></i><span>MinecraftGolden</span></a
-            >
-            <a href="cabin.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item">
-                <i class="fas fa-heartbeat fa-fw me-3"></i><span>The Cabin</span></a
-            >
-            <a href="haven.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item">
-                <i class="fas fa-code fa-fw me-3"></i><span>The Haven</span></a
-            >
-            <a href="credit.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
-              ><i class="fas fa-award fa-fw me-3"></i><span>Credit</span></a
-            >
-            <a href="legal.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
-              ><i class="fas fa-balance-scale fa-fw me-3"></i><span>Legal</span></a
-            >
-            <!--
-            <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-              ><i class="fas fa-money-bill fa-fw me-3"></i><span>Sales</span></a
-            > -->
-          </div>
-        </div>
-      </nav>
-      <!-- Sidebar -->
-    
-      <!-- Navbar -->
-      <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
-        <!-- Container wrapper -->
-        <div class="container-fluid">
-          <!-- Toggle button -->
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-mdb-toggle="collapse"
-            data-toggle="collapse"
-            data-mdb-target="#sidebarMenu"
-            data-target="#collapseOne"
-            aria-controls="sidebarMenu"
-            aria-expanded="true"
-            aria-label="Toggle navigation"
+<!--Main Navigation-->
+<header>
+  <!-- Sidebar -->
+  <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-dark">
+    <div class="position-sticky">
+      <div class="list-group list-group-flush mx-3 mt-4">
+        <a
+          href="index.php"
+          class="list-group-item list-group-item-action py-2 ripple bg-button"
+          aria-current="true"
+        >
+          <i class="fas fa-address-card fa-fw me-3"></i><span>About us</span></a
           >
-            <i class="fas fa-bars"></i>
-          </button>
-    
-        <!-- data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> -->
-    
-          <!-- Brand -->
-          <a class="navbar-brand" href="index.php">
-            <img
-              src="assets/PsychOpsTransparent.png"
-              class="navbar-ps"
-              height="25"
-              alt=""
-              loading="lazy"
-            />
+        <a href="team.php" class="list-group-item list-group-item-action py-2 ripple bg-button">
+          <i class="fas fa-users fa-fw me-3"></i><span>Our team</span></a
+          >
+        <a href="positions.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
+          ><i class="fas fa-laptop-code fa-fw me-3"></i><span>Open positions</span></a
+        >
+        <a href="associates.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
+          ><i class="fas fa-handshake fa-fw me-3"></i><span>Associates</span></a
+        >
+        <a href="commissions.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
+          ><i class="fas fa-coins fa-fw me-3"></i><span>Commissions</span></a
+        >
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item-head"
+          ><i class="fas fa-project-diagram fa-fw me-3"></i><span>Projects</span><i class="fas fa-sort-down"></i></a
+        >
+        <a href="pingernos.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item"
+          ><i class="fas fa-server fa-fw me-3"></i><span>Pingernos</span></a
+        >
+        <a href="varumox.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item"
+          ><i class="fas fa-server fa-fw me-3"></i><span>Varumox</span></a
+        >
+        <a href="network.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item-head"
+          ><i class="fas fa-network-wired fa-fw me-3"></i><span>Network</span><i class="fas fa-sort-down"></i></a
+        >
+        <a href="badwolf.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item">
+            <i class="fas fa-paw fa-fw me-3"></i><span>Bad Wolf</span></a
+        >
+        <a href="mg.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item">
+          <i class="fas fa-cubes fa-fw me-3"></i><span>MinecraftGolden</span></a
+        >
+        <a href="cabin.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item">
+            <i class="fas fa-heartbeat fa-fw me-3"></i><span>The Cabin</span></a
+        >
+        <a href="haven.php" class="list-group-item list-group-item-action py-2 ripple bg-button ntw-item">
+            <i class="fas fa-code fa-fw me-3"></i><span>The Haven</span></a
+        >
+        <a href="credit.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
+          ><i class="fas fa-award fa-fw me-3"></i><span>Credit</span></a
+        >
+        <a href="legal.php" class="list-group-item list-group-item-action py-2 ripple bg-button"
+          ><i class="fas fa-balance-scale fa-fw me-3"></i><span>Legal</span></a
+        >
+        <!--
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-money-bill fa-fw me-3"></i><span>Sales</span></a
+        > -->
+      </div>
+    </div>
+  </nav>
+  <!-- Sidebar -->
+
+  <!-- Navbar -->
+  <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-toggle="collapse"
+        data-mdb-target="#sidebarMenu"
+        data-target="#collapseOne"
+        aria-controls="sidebarMenu"
+        aria-expanded="true"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+
+    <!-- data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> -->
+
+      <!-- Brand -->
+      <a class="navbar-brand" href="index.php">
+        <img
+          src="assets/PsychOpsTransparent.png"
+          class="navbar-ps"
+          height="25"
+          alt=""
+          loading="lazy"
+        />
+      </a>
+      <!-- Right links -->
+      <ul class="navbar-nav ms-auto d-flex flex-row">
+        <!-- Icon -->
+        <li class="nav-item me-3 me-lg-0">
+          <a class="nav-link" href="https://github.com/PsychOps" target="_blank">
+            <i class="fab fa-github bg-button fa-2x" aria-hidden="true"></i>
           </a>
-          <!-- Right links -->
-          <ul class="navbar-nav ms-auto d-flex flex-row">
-            <!-- Icon -->
-            <li class="nav-item me-3 me-lg-0">
-              <a class="nav-link" href="https://github.com/PsychOps" target="_blank">
-                <i class="fab fa-github bg-button fa-2x" aria-hidden="true"></i>
-              </a>
-            </li>
-          </ul>
-      </nav>
-      <!-- Navbar -->
-    </header>
-    <!--Main Navigation-->
-    
-    <!--Main layout-->
-    <main style="margin-top: 58px;">
-      <div class="container pt-4"></div>
-    </main>
-    <!--Main layout-->
+        </li>
+      </ul>
+  </nav>
+  <!-- Navbar -->
+</header>
+<!--Main Navigation-->
+
+<!--Main layout-->
+<main style="margin-top: 58px;">
+  <div class="container pt-4"></div>
+</main>
+<!--Main layout-->
---
+++ varumox.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/html/head.html' ;?>
+
+<?php include 'templates/html/navigation.php' ;?>
+
+<body>
+
+<div class="notnavtext text-center">
+    <p class="fs-1">Varumox</p>
+    <p>
+    The website and API for Varumox Marketplace - quality plugins, at no cost. Coming soon.
+    </p>
+    <a href="https://discord.gg/QMhvHuGPMx" target="_blank" class="btn bg-green btn-rounded">Central Discord Server</a>
+</div>
+</body>
+
+</html>
