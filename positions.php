00343a393371621a048ffd89fff0b9d10dd474ea
joshua <zoniqhedgehog@gmail.com>
2021-12-03 20:56:21 +0100

    Developer positions open!!!

--- positions.php
+++ positions.php
@@ -8,12 +8,140 @@
 <body>
 
 <div class="notnavtext text-center">
-    <img src="/assets/S1.png" alt="">
-    <p class="fs-1">Positions</p>
-    <p>
-        There are currently no open positions, sorry!<br/>Want to know when positions open? Join our discord server!
-    </p>
-    <a href="https://discord.gg/QMhvHuGPMx" target="_blank" class="btn bg-green btn-rounded">Central discord server</a>
+    <div class="container">
+        <img src="/assets/S1.png" alt="">
+        <p class="fs-1">Job offer - Developer (english)</p>
+        <p class="subtitle"> <i>Scroll down for the dutch (Nederlands) version.</i> </p>
+    </div>
+</div>
+
+<div class="notnavtext text-center sectionstart">
+
+    <div class="container">
+        <a href="mailto:psychopsorg@gmail.com" target="_blank" class="btn bg-green btn-rounded">Mail us your application</a>
+        <p> <i> A formal job application is required. Additionally, please send in your resume if you have one. </i> </p>
+    </div>
+
+    <div class="container">
+        <p>
+            PsychOps Development is looking for new developers to join our team. Our goal is to provide in free and open-source products that are available for anyone to use without (m)any issues. We want our developers to share in the motivation for that goal. <b>Team members at PsychOps do not get paid, please do not apply for this job just for money.</b>
+        </p>
+    </div>
+
+    <div class="container">
+        <p>
+        We are looking for developers that have passion, motivation and the will to do both large- and small scale projects in name of PsychOps. The main focus lies with user satisfaction, and we will always look out for experienced developers that share our focus and will to make projects for the sake of our users, but this is not the only requirement to become a developer at PsychOps. Please read all our requirements down below if you wish to apply for the job.
+        </p>
+    </div>
+</div>
+
+<div class="notnavtext sectionstart">
+    <div class="container">
+        <p class="fs-3">Requirements</p>
+        <lu>
+            <li>Basic knowledge in Javascript <b>or</b> Python</li>
+            <li>Basic knowledge in HTML and CSS</li>
+            <li>Must be over 13 years old</li>
+            <li>Must be fluent in english</li>
+            <li>Must have Discord or be open to getting it.</li>
+            <li>Must be prepared to be active in and around PsychOps</li>
+            <li>Must present yourself as friendly, welcoming and supportive</li>
+            <li>Must not have a bad reputation within PsychOps, affiliates, partners and/or our products</li>
+            <li>Must not be banned on <a href="https://azrael.gg" target="_blank">azrael.gg</a></li>
+        <lu>
+    </div>
+</div>
+
+<div class="notnavtext sectionstart">
+    <div class="container">
+        <p> <b>These requirements are only the minimal and will not guarantee that we hire you. Our application reviews are discussed internally based on the quality of your application ADDITIONALLY to whether you meet the requirements. We do not strictly look at whether an applicant also possesses the preferred skills, but we would always appreciate applicants possessing (some of) these.</b> </p>
+    </div>
+</div>
+
+<div class="notnavtext sectionstart">
+    <div class="container">
+        <p class="fs-3">Preferred</p>
+        <lu>
+            <li>6+ months of experience in Javascript <b>or</b> Python</li>
+            <li>Fluently speak dutch</li>
+            <li>Have experience with Linux systems</li>
+        <lu>
+    </div>
+</div>
+
+<div class="notnavtext sectionstart">
+    <div class="container">
+        <p>Lastly, PsychOps wishes to maintain a good and proper image with its users and does not wish to take part in anything that may contradict european law, the discord ToS, our own privacy policy or other legal issues that may affect PsychOps as organization. If applicants are prone to causing such issues, the application will be declined immediately. We will do thorough checks on our applicants to make sure their applications are honest, and that they may not cause issues for our organization at a later moment.</p>
+    </div>
+</div>
+
+<!-- THE DUTCH VERSION STARTS HERE. -->
+
+<div class="notnavtext text-center">
+    <div class="container">
+        <hr>
+        <p class="fs-1">Vacature Developer (Nederlands)</p>
+        <p class="subtitle"> <i>Scroll omhoog voor de engelse versie.</i> </p>
+    </div>
+</div>
+
+<div class="notnavtext text-center sectionstart">
+
+    <div class="container">
+        <a href="mailto:psychopsorg@gmail.com" target="_blank" class="btn bg-green btn-rounded">Mail uw sollicitatie naar ons</a>
+        <p> <i> Een formele sollicitatiebrief is vereist. Indien u een CV (Curriculum Vitae) hebt, willen wij deze ook graag ontvangen.  </i> </p>
+    </div>
+
+    <div class="container">
+        <p>
+            PsychOps Development zoekt naar nieuwe developers om ons team te versterken. Ons doel is om gratis en open-source producten vrij te geven aan iedereen, met geen, of weinig problemen. Wij willen dat onze developers onze motivatie deelt voor dit doel. <b>PsychOps teamleden worden niet betaald aangezien onze producten compleet gratis zijn. Solliciteer niet voor deze baan om geld te ontvangen.</b>
+        </p>
+    </div>
+
+    <div class="container">
+        <p>
+        Wij zoeken voor developers met passie, motivatie en de wil om aan groot- en kleinschalige projecten mee te doen in de naam van PsychOps. De algemene focus ligt bij de tevredenheid van onze gebruikers, dusdanig zoeken wij ervaren developers die deze focus en motivatie deelt om projecten te ontwikkelen met gebruiksvriendelijkheid als doel. Dit is natuurlijk niet de enige eis die wij stellen. Lees alstublieft al onze eisen hieronder als u voor deze baan wilt solliciteren.
+        </p>
+    </div>
+</div>
+
+<div class="notnavtext sectionstart">
+    <div class="container">
+        <p class="fs-3">Onze eisen</p>
+        <lu>
+            <li>Basiskennis Javascript en/of Python</li>
+            <li>Basiskennis HTML en CSS</li>
+            <li>Moet 13 jaar of ouder zijn</li>
+            <li>Moet vloeiend engels spreken</li>
+            <li>Moet Discord hebben of bereid zijn Discord te nemen</li>
+            <li>Moet bereid zijn actief in, en rondom PsychOps te zijn/worden</li>
+            <li>Moet bekend staan als een vriendelijk, welkomend persoon</li>
+            <li>Moet niet op <a href="https://azrael.gg" target="_blank">azrael.gg</a> verbannen zijn</li>
+        <lu>
+    </div>
+</div>
+
+<div class="notnavtext sectionstart">
+    <div class="container">
+        <p> <b>Deze eisen zijn alleen het minimum, en garanderen niet dat we u aannemen. De oordeling over sollicitaties worden intern gediscussieerd gebaseerd op de kwaliteit van uw sollicitatie, met daarnaast of u aan de eisen voldoet. Wij kijken niet strict naar of een sollicitant ook de gewenste vaardigheden bezit, maar dit kan in uw voordeel zijn bij uw sollicitatie.</b> </p>
+    </div>
+</div>
+
+<div class="notnavtext sectionstart">
+    <div class="container">
+        <p class="fs-3">Gewenst</p>
+        <lu>
+            <li>6+ Maanden ervaring in JavaScript en/of Python</li>
+            <li>Vloeiend nederlands spreken</li>
+            <li>Ervaring hebben met Linux systemen</li>
+        <lu>
+    </div>
+</div>
+
+<div class="notnavtext sectionstart">
+    <div class="container">
+        <p>Ten slotte, PsychOps wenst een goed en respecterend beeld op te houden bij zijn gebruikers, en doet niet mee aan het breken van Europese wetten, de Discord ToS, onze eigen privacy policy, of enig andere legale problemen die PsychOps als organizatie kunnen schaden. Als sollicitanten duidelijk een probleem kunnen vormen in relatie tot dit, zal de sollicitatie meteen verwijderd en afgewezen worden. Wij zullen altijd grondig onderzoeken of een sollicitant geschikt is, of zij een probleem kunnen vormen voor PsychOps op een later moment, en of de sollicitaties eerlijk, en feitelijk zijn.</p>
+    </div>
 </div>
 
 </body>
