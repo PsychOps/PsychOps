27545403aeb7b953376809712feb2b95e951217f
joshua <zoniqhedgehog@gmail.com>
2021-12-12 20:13:06 +0100

    Correct href

--- associates.php
+++ associates.php
@@ -17,7 +17,7 @@
         <br/>
         Associates work narrowly together with PsychOps to support each other in the development of products, creating a community or investing into (nearly) abandoned projects in the belief that it may be useful to people. Feel like you, your company or your organization fit this description? Be sure to email us!
     </p>
-    <a href="https://mail.google.com/mail/u/0/?fs=1&to=psychopsorg@gmail.com&su=Apply%20for%20partner&tf=cm" target="_blank" class="btn bg-green btn-rounded">Contact us</a>
+    <a href="mailto:psychopsorg@gmail.com" target="_blank" class="btn bg-green btn-rounded">Contact us</a>
 </div>
 
 </body>
