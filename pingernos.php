f9fcba2e765a38ff79e1ae639e811ce779549802
joshua <zoniqhedgehog@gmail.com>
2021-12-12 20:16:59 +0100

    Remove linebreaks

--- pingernos.php
+++ pingernos.php
@@ -17,8 +17,7 @@
         <br/>
         You can use <code><@889197952994791434> status [Aternos name or IP]</code> to grab the status of the given Aternos server. The API we use is not affiliated with Aternos, Minecraft or any server host. This bot is also not affiliated with Aternos.
     </p>
-    <br/>
-    <br/>
+
     <a href="https://discord.com/api/oauth2/authorize?client_id=889197952994791434&permissions=274878286848&scope=bot" target="_blank" class="btn bg-green btn-rounded">Invite it here!</a>
     <a href="https://discord.gg/QMhvHuGPMx" target="_blank" class="btn bg-green btn-rounded">Central Discord Server</a>
 </div>
