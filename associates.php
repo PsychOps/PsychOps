76fcb92af51a0b058f90ff983ee4ae2456b194ad
joshua <zoniqhedgehog@gmail.com>
2021-12-12 20:12:59 +0100

    Better info about associating with PsychOps

--- associates.php
+++ associates.php
@@ -15,7 +15,7 @@
     <p>
         PsychOps does not have any associates yet.
         <br/>
-        Want to apply for the role? Send us a mail!
+        Associates work narrowly together with PsychOps to support each other in the development of products, creating a community or investing into (nearly) abandoned projects in the belief that it may be useful to people. Feel like you, your company or your organization fit this description? Be sure to email us!
     </p>
     <a href="https://mail.google.com/mail/u/0/?fs=1&to=psychopsorg@gmail.com&su=Apply%20for%20partner&tf=cm" target="_blank" class="btn bg-green btn-rounded">Contact us</a>
 </div>
