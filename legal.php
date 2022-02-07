4037658afc44cb9363130b40182bb7e6afb19772
joshua <zoniqhedgehog@gmail.com>
2022-02-07 17:53:45 +0100

    Legal stuff

--- legal.php
+++ legal.php
@@ -35,6 +35,40 @@
             The legal page will mostly inform our users about copyright and/or trademarks concerning PsychOps Studios, partners and/or affiliates. If any questions occur or vital information is missing on this page, please contact us at your earliest convenience with <a href="mailto:psychopsorg@gmail.com">psychopsorg@gmail.com</a>.
         </p>
     </div>
+
+    <div class="sectionstart textend">
+        <hr>
+        <div class="sectionstart">
+            <h1 class="fs-2">Logos</h1>
+            <p>
+                PsychOps Studios makes use of logos (referred to as "images") created by its own graphic designers and with consent from third parties. PsychOps Studios does not however take ownership of these images by own initiative. This means that the images will remain property of the graphic designers that PsychOps Studios has instructed. Additionally, PsychOps Studios will not claim any affiliations and/or ownership to/of third parties that we consensually use images from unless the affiliation and/or ownership is factual. You can find the lawful owners of the used images below. If you wish to see the involved laws, or relative images, please check out the "external links" tab in the top right of the page.
+            </p>
+
+            <div id="Aternos" class="textcard">
+                <img src="https://aternos.gmbh/img/logotype-light.svg" alt="" height="50px" style="padding-bottom: 10px">
+                <p><a href="/pingernos.php">Pingernos</a> makes use of the <a href="https://aternos.org">Aternos</a> logo. Any and all rights and credits to this logo goes to them. Please follow <a href="https://aternos.gmbh/en/branding">this link</a> for more information.</p>
+            </div>
+
+            <div id="PsychOps" class="textcard">
+                <img src="/assets/S1.png" alt="" height="50px" style="padding-bottom: 10px">
+                <img src="/assets/PsychOpsTransparent.png" alt="" height="50px" style="padding-bottom: 10px">
+                <p>PsychOps Studios makes use of the PsychOps logos (1, 2). Any and all rights and credits to this logo goes to Amgelo.</p>
+            </div>
+
+            <div id="Varumox" class="textcard">
+                <!--<img src="/assets/images/Varumox%20V.png" alt="" height="50px" style="padding-bottom: 10px">-->
+                <img src="/assets/images/Varumox.png" alt="" height="50px" style="padding-bottom: 10px">
+                <p>Varumox makes use of the Varumox logos (1, 2). Any and all rights and credits to this logo goes to Manuel Maiani.</p>
+            </div>
+
+            <div id="Modraxis" class="textcard">
+                <!--<img src="/assets/images/Varumox%20V.png" alt="" height="50px" style="padding-bottom: 10px">-->
+                <img src="/assets/images/Modraxis.png" alt="" height="50px" style="padding-bottom: 10px">
+                <p>Modraxis (logos 1, 2) is currently not a logo in use but was created for PsychOps Studios. Any and all rights and credits to this logo goes to Amgelo.</p>
+            </div>
+
+        </div>
+    </div>
 </div>
 
 </body>
