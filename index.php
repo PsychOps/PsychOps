bbd0bb7ca053cfab546050e54f1b50af4a7cd7ac
joshua <zoniqhedgehog@gmail.com>
2022-02-07 17:52:05 +0100

    Change information about PS

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
     <img src="/assets/S1.png" alt="">
     <h1>About PsychOps</h1>
     <p>
-        PsychOps is a Netherlands based organization owned by <a href="https://zoniq.tk" target="_blank">Joshua</a> and <a href="https://aboutjay.tk/" target="_blank">Jay</a> dedicated to open-source software and web development with <b>3</b> developers, and an additional <b>3</b> staff members. With user experience and feedback in mind, PsychOps Development always attempts to provide its users with products that are to their liking.
-        Additionally, PsychOps also functions as a hub for developers of any field. With our main discord server, we allow getting support for any coding language and our products, from both the community as well as the staff team at PsychOps.
+        PsychOps Studios is a Netherlands based organization owned by <a href="https://zoniq.tk" target="_blank">Joshua</a> and <a href="https://hilex23.tk/" target="_blank">Manuel</a> dedicated to open-source software and web development with <b>5</b> developers, including the owners. With user experience and feedback in mind, PsychOps Studios always attempts to provide its users with products that are to their liking.
+        Additionally, PsychOps also functions as a hub for developers of any field. With our main discord server, we allow getting support for any coding language and our products, from both the community as well as the staff team at PsychOps. <b>PsychOps Studios does not take commissions in compliance to dutch laws.</b>
     </p>
     <a href="https://discord.gg/QMhvHuGPMx" target="_blank" class="btn bg-green btn-rounded">Join our discord</a>
 </div>
