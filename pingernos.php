8cd8bbb474a25f8e8666d6c11658e3c0e773181a
ToothyDev <55001472+ToothyDev@users.noreply.github.com>
2022-02-13 22:22:11 +0100

    Add Discord icon

--- pingernos.php
+++ pingernos.php
@@ -19,7 +19,7 @@
     </p>
 
     <a href="https://discord.com/api/oauth2/authorize?client_id=889197952994791434&permissions=274878286848&scope=bot" target="_blank" class="btn bg-green btn-rounded">Invite it here!</a>
-    <a href="https://discord.gg/QMhvHuGPMx" target="_blank" class="btn bg-green btn-rounded">Central Discord Server</a>
+    <a href="https://discord.gg/QMhvHuGPMx" target="_blank" class="btn bg-green btn-rounded"><span><i class="fab fa-discord" aria-hidden="true"></i></span> Central Discord Server</a>
 </div>
 
 </body>
