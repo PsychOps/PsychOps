993bb3893335ac28f2b6a8519a5e7cfee6e09b8b
joshua <joshuaslui0203@gmail.com>
2022-04-26 19:23:32 +0200

    Change email

--- associates.php
+++ associates.php
@@ -13,7 +13,7 @@
     <p>
         Associates work narrowly together with PsychOps to support each other in the development of products, creating a community or investing into (nearly) abandoned projects in the belief that it may be useful to people. Feel like you, your company or your organization fit this description? Be sure to email us!
     </p>
-    <a href="mailto:psychopsorg@gmail.com" target="_blank" class="btn bg-green btn-rounded">Contact us</a>
+    <a href="mailto:contact@psychops.tk" target="_blank" class="btn bg-green btn-rounded">Contact us</a>
 </div>
 
 <div class="container text-center">
