bf99988d235eaafe2bcddd1f3b761994386af937
Hilex23 <89878821+Hilex23@users.noreply.github.com>
2022-05-24 18:07:32 +0200

    make supporters page and clean team and privacy page

--- team.php
+++ team.php
@@ -7,67 +7,67 @@
 
 <body>
 
-<div class="team container text-center">
-    <h1 class="main-title">Our Team</h1>
-    <p class="subtext-2">There are currently 5 developers working at PsychOps and a total of 8 team members.</p>
-    <div class="team-row">
-        <div class="member">
-            <a href="https://zoniq.tk" target="_blank"><img src="assets/images/team/joshua.gif" class="owner" alt=""></a>
-            <h2>Spyro (Joshua)</h2>
-            <p>Owner</p>
-            <a href="https://github.com/DeltaSLM" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-            <a href="https://twitter.com/JamesStrEVs" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-            <a href="https://discord.com/users/839237573595365406" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-        </div>
-        <div class="member">
-            <a href="https://heelys32.ml" target="_blank"><img src="assets/images/team/manuel.png" class="owner" alt=""></a>
-            <h2>Heelyx23 (Manuel)</h2>
-            <p>Owner</p>
-            <a href="https://github.com/Hilex23" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-            <a href="https://twitter.com/hilex23" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-            <a href="https://discord.com/users/370258256775479307" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-        </div>
-        <div class="member">
-            <img src="assets/images/team/jimi.png" class="executive" alt="">
-            <h2>Jimi6505</h2>
-            <p>Executive</p>
-        </div>
-        <div class="member">
-            <img src="assets/images/team/toothy.png" class="executive" alt="">
-            <h2>Toothy Fernsan</h2>
-            <p>Executive Developer</p>
-            <a href="https://github.com/ToothyDev" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-            <a href="https://discord.com/users/341988909363757057" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-        </div>
-        <div class="member">
-            <img src="assets/images/team/somebody.png" class="project-manager" alt="">
-            <h2>Somebody</h2>
-            <p>Project Manager</p>
-            <a href="https://discord.com/users/438049859262480386" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-        </div>
-        <div class="member">
-            <img src="assets/images/team/aresiel.gif" class="team-dev" alt="">
-            <h2>Aresiel</h2>
-            <p>Web Developer</p>
-            <a href="https://github.com/Aresiel" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-            <a href="https://discord.com/users/104933285506908160" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-        </div>
-        <div class="member">
-            <a href="https://dobbie.tk/" target="_blank"><img src="assets/images/team/dobbie.jpg" class="team-dev" alt=""></a>
-            <h2>Dobbie</h2>
-            <p>Developer</p>
-            <a href="https://github.com/Dobby-progam" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-            <a href="https://discord.com/users/820255805257023498" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-        </div>
-        <div class="member">
-            <img src="assets/images/team/matt.png" class="social-manager" alt="">
-            <h2>MattGold</h2>
-            <p>Social Media Manager</p>
-            <a href="https://github.com/MattGold-5829" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
-            <a href="https://twitter.com/MattGold_" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+    <div class="team container text-center">
+        <h1 class="main-title">Our Team</h1>
+        <p class="subtext-2">There are currently 5 developers working at PsychOps and a total of 8 team members.</p>
+        <div class="team-row">
+            <div class="member">
+                <a href="https://zoniq.tk" target="_blank"><img src="assets/images/team/joshua.gif" class="owner" alt=""></a>
+                <h2>Spyro (Joshua)</h2>
+                <p>Owner</p>
+                <a href="https://github.com/DeltaSLM" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/JamesStrEVs" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/839237573595365406" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <a href="https://heelys32.ml" target="_blank"><img src="assets/images/team/manuel.png" class="owner" alt=""></a>
+                <h2>Heelyx23 (Manuel)</h2>
+                <p>Owner</p>
+                <a href="https://github.com/Hilex23" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/hilex23" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/370258256775479307" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="assets/images/team/jimi.png" class="executive" alt="">
+                <h2>Jimi6505</h2>
+                <p>Executive</p>
+            </div>
+            <div class="member">
+                <img src="assets/images/team/toothy.png" class="executive" alt="">
+                <h2>Toothy Fernsan</h2>
+                <p>Executive Developer</p>
+                <a href="https://github.com/ToothyDev" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/341988909363757057" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="assets/images/team/somebody.png" class="project-manager" alt="">
+                <h2>Somebody</h2>
+                <p>Project Manager</p>
+                <a href="https://discord.com/users/438049859262480386" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="assets/images/team/aresiel.gif" class="team-dev" alt="">
+                <h2>Aresiel</h2>
+                <p>Web Developer</p>
+                <a href="https://github.com/Aresiel" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/104933285506908160" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <a href="https://dobbie.tk/" target="_blank"><img src="assets/images/team/dobbie.jpg" class="team-dev" alt=""></a>
+                <h2>Dobbie</h2>
+                <p>Developer</p>
+                <a href="https://github.com/Dobby-progam" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/820255805257023498" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="assets/images/team/matt.png" class="social-manager" alt="">
+                <h2>MattGold</h2>
+                <p>Social Media Manager</p>
+                <a href="https://github.com/MattGold-5829" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/MattGold_" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
         </div>
     </div>
-</div>
 
     <script src="assets/js/navbar.js"></script>
 
