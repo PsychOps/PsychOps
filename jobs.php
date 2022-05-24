098c83a713a569746e55d200a3839b651d210bb9
joshua <joshuaslui0203@gmail.com>
2022-05-24 19:27:42 +0200

    Job page

---
+++ jobs.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/head.html' ;?>
+
+<?php include 'templates/navbar.html' ;?>
+
+<body>
+
+<div class="whiteground text-center">
+    <div class="container">
+        <h1 class="main-title">Job vacancies</h1>
+        <p class="main-text">We at PsychOps Studios are actively looking for people supporting our goals who willing to work on making them real. We can't do this on our own, and not with a small team. This is where we call upon you to join our team and help us out!</p>
+    </div>
+</div>
+
+<div class="blackground">
+    <div class="container">
+        <h1>Job positions</h1>
+        <div class="list">
+            <div class="entry entry-success">Developer</div>
+            <div class="entry entry-danger">Executive positions</div>
+            <div class="entry entry-danger">Social Media Manager</div>
+        </div>
+    </div>
+</div>
+
+<div class="whiteground">
+    <div class="container">
+        <h1>Others</h1>
+        Additionally to the job vacancies listed above, we also offer a few positions in our organization that do not include any of the obligations and, or tasks that are relevant to the actual jobs. If you are interested in one of the below listed positions, feel free to contact us at <a href="mailto:contact@psychops.tk">contact@psychops.tk</a> with proof of your work and what you'd like to be. We welcome suggestions and feedback on positions within PsychOps Studios actively.
+        <br> <br>
+        <ul>
+            <li>Graphic Designer</li>
+        </ul>
+    </div>
+</div>
+
+<div>
+    <div class="container">
+        <a href="about.php" class="btn btn-info">Looking to partner instead?</a>
+    </div>
+</div>
+
+<script src="assets/js/navbar.js"></script>
+
+</body>
+
+</html>
