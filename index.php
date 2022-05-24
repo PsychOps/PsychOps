a500bf45b506d051c9dc03684c1c731e9d388f02
joshua <joshuaslui0203@gmail.com>
2022-05-24 12:25:08 +0200

    add alt, edit description slightly

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 
     <div class="home container text-center">
         <h1 class="main-title">PsychOps Studios</h1>
-        <img src="/assets/images/psychops/logo.png" class="home-logo">
-        <p class="main-text">PsychOps Studios, a producer of completely free, open source user-friendly products</p>
+        <img src="/assets/images/psychops/logo.png" class="home-logo" alt="">
+        <p class="main-text">A producer of completely free, open source user-friendly products</p>
         <div class="txt-btn-div">
             <a href="about.php" class="txt-btn">About Us</a>
         </div>
