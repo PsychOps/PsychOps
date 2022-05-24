4be633d6148bc1205521e0ef719f3034c6dd1508
joshua <joshuaslui0203@gmail.com>
2022-05-24 15:03:34 +0200

    Add privacy page

---
+++ privacy.php
@@ -0,0 +1,126 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/head.html' ;?>
+
+<?php include 'templates/navbar.html' ;?>
+
+<body>
+
+<div class="home container">
+    <h1 class="main-title text-center">Privacy policy</h1>
+</div>
+
+<div class="whiteground">
+    <div class="container">
+        <div class="box">
+            <h2 class="text-center">Contents</h2>
+            <ul>
+                <li> <a href=#collected>Information we collect and how we use it</a> </li>
+                <li> <a href=#how>How we collect data</a> </li>
+                <li> <a href=#children>Children under the age of 16</a> </li>
+                <li> <a href=#changes>Privacy Policy changes</a> </li>
+                <li> <a href=#removal>Data removal requests</a> </li>
+                <li> <a href=#rights>User Rights</a> </li>
+                <li> <a href=#contact>Contact Us</a> </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="blackground" id="collected">
+    <div class="container">
+        <h2>Information we collect and how we use it</h2>
+        <p>
+            We gather a number of things to ensure our products work as they are supposed to. Please click the buttons below to see what data is gathered per product.
+        </p>
+
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                        Pingernos
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        Pingernos gathers the below listed data. <b>These are only gathered upon correct execution of the <code>set</code> command.</b>
+                        <br>You can get the entries in the database removed by using <code>set delete</code> in the correct discord server.
+                        <ul>
+                            <li>Guild IDs</li>
+                            <li>Server IPs</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        PsychUtils
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        PsychUtils gathers the below listed data. <b>These are only gathered upon correct execution of the currency commands.</b>
+                        <br><b>You can not get these data entries deleted on your own. Refer to the <a href="#removal">data removal</a> paragraph.</b>
+                        <ul>
+                            <li>user ID (all currency commands, cooldown)</li>
+                            <li>wallet (all currency commands)</li>
+                            <li>bank (all currency commands)</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="whiteground" id="how">
+    <div class="container">
+        <h2>How we collect data</h2>
+        <p>The data is simply gathered by our bots and sent to the mysql database on the server. The data is safely stored there, only accessible by PsychOps Studios and the server provider. <b>This data will never be shared with, or sold to third parties. Your privacy is important to us.</b> Without collecting this data, our products will not function, please do not ask us to disable data collection for your account(s).</p>
+    </div>
+</div>
+
+<div class="blackground" id="children">
+    <div class="container">
+        <h2>Children under the age of 16</h2>
+        <p>According to <a href="https://gdpr-info.eu/art-8-gdpr/">GDPR Article 8</a>, it is not legally allowed to gather data of users under 16 without their guardian's explicit consent. We do not knowingly gather data of people below 16, and our website is not dedicated to anyone under that age. If you are below this age, please consider clearing your cookies, cache and closing this tab and/or requesting removal of your data and refraining from using our products if you do not have parental consent.</p>
+    </div>
+</div>
+
+<div class="whiteground" id="changes">
+    <div class="container">
+        <h2>Privacy policy changes</h2>
+        <p>We reserve the right to change our Privacy Policy should this be necessary to inform users what data may be newly, or no longer collected through any of our public products. Might this be the case, a notice will be sent out to our users through each of our social media platforms prior to the update. We do not hold any responsibility to users who may or may not have seen the notice prior to the Privacy Policy being adequately changed.</p>
+    </div>
+</div>
+
+<div class="blackground" id="removal">
+    <div class="container">
+        <h2>Data removal requests</h2>
+        <p>Our users reserve the right to request the removal of any data that we may or may not have saved in our database(s). Requests can be submitted through <a href="mailto:support@psychops.tk">support@psychops.tk</a>. Please <b>provide your discord user ID</b> and for Pingernos <b>the guild ID</b>. Without either of these two, we cannot comply with your request. Data removal can take up to 30 days, and an e-mail will be sent back upon completion.</p>
+    </div>
+</div>
+
+<div class="whiteground" id="rights">
+    <div class="container">
+        <h2>User Rights</h2>
+        <p>By law, our users reserve the right to know what data has been collected and stored in our database(s), as well as the right to get their data removed as described above. We accept inquiries for these requests at <a href="mailto:support@psychops.tk">support@psychops.tk</a> and nowhere else, for privacy concerns. Please note that we will not comply with requests to see and, or remove data belonging to other individuals which are not yourself. Additionally, when inquiring, you are required to provide proof that you are in fact the person to whom the data belongs.</p>
+        <p>Furthermore, according to <a href="https://gdpr-info.eu/art-8-gdpr/">GDPR</a>, you have the right to lodge a complaint with a supervisory authority if you believe that the processing of your personal data is illicit.</p>
+        <p>You are only authorized to agree to this policy if you are at least 16 years old (or less if your country has different regulations) or if consent is given by the holder of your parental responsibility. You can read the <a href=#children>"Children under the age of 16"</a> section for more info. PsychOps Studios does not hold itself accountable for unwarranted data collection of anyone younger than the minimal age of consent for data collection.</p>
+    </div>
+</div>
+
+<div class="blackground" id="contact">
+    <div class="container">
+        <h2>Contact us</h2>
+        <p>You can contact us for concerns, questions and inquiries at <a href="mailto:contact@psychops.tk">contact@psychops.tk</a>. If you wish to send an e-mail privately, you may do so through <a href="mailto:joshua@psychops.tk">joshua@psychops.tk</a> or <a href="mailto:manuel@psychops.tk">manuel@psychops.tk</a>. If you are merely looking for support or are seeking for alternative ways to get support, you may try the "contact us" dropdown in our navigation bar.</p>
+    </div>
+</div>
+
+<script src="assets/js/navbar.js"></script>
+
+</body>
+
+</html>
