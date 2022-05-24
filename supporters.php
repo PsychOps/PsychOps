5a676372f92cff59a62b07e0641ebbe18734333c
Hilex23 <89878821+Hilex23@users.noreply.github.com>
2022-05-24 18:29:13 +0200

    fixed supporters page

---
+++ supporters.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/head.html' ;?>
+
+<?php include 'templates/navbar.html' ;?>
+
+<body>
+
+    <div class="team container text-center">
+        <h1 class="main-title">Our Supporters</h1>
+        <p class="main-text">We currently have 3 graphic designers that help us by creating amazing logos, banners, and much more.</p>
+        <div class="team-row">
+            <div class="member">
+                <img src="assets/images/supporters/amgelo.png" class="graphic-designer" alt="">
+                <h2>Amgelo</h2>
+                <p>Graphic Designer</p>
+                <a href="https://github.com/Amgelo563" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/235428738748121088" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="assets/images/team/somebody.png" class="graphic-designer" alt="">
+                <h2>Somebody</h2>
+                <p>Graphic Designer</p>
+                <a href="https://discord.com/users/438049859262480386" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="assets/images/supporters/bigchez.png" class="graphic-designer" alt="">
+                <h2>Big Chez</h2>
+                <p>Graphic Designer</p>
+                <a href="https://twitter.com/carlwife42" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/584672380085862407" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <script src="assets/js/navbar.js"></script>
+
+</body>
+
+</html>
