0a00b0679f5bdec3d48f97b41ccddddee7b9aa5c
joshua <joshuaslui0203@gmail.com>
2022-05-24 21:37:09 +0200

    Update TPK partner page

--- tpk.php
+++ tpk.php
@@ -7,14 +7,40 @@
 
 <body>
 
-<div class="about container text-center page-center">
-    <h1 class="main-title">The Patris Kingdom</h1>
-    <p class="main-text">
-        This page is currently in the works. Join their discord while you wait!
-    </p>
+<div class="whiteground text-center">
     <div class="container">
-        <iframe src="https://discord.com/widget?id=715969701771083817&theme=dark" width="100%" height="500" allowtransparency="true" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        <h1 class="main-title">The Patris Kingdom</h1>
+        <p class="main-text">
+            The Patris Kingdom is a loving community that wishes to bring harmony and unity to furries, the LGBTQ+ community and other people. We want to give people the best of what we can offer, and will always try to accommodate them the best we can.
+        </p>
     </div>
+</div>
+
+<div class="blackground">
+    <div class="container team" style="padding: 0">
+        <div class="team-row" style="padding: 0">
+            <div class="member">
+                <img src="assets/images/team/toothy.png" class="owner" alt="">
+                <h2 style="color: var(--main)">Toothy</h2>
+                <p style="color: var(--main)">Owner</p>
+                <a href="https://github.com/ToothyDev" target="_blank"><i class="fab fa-github" style="color: var(--main); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/chosenfate_dx" target="_blank"><i class="fab fa-twitter" style="color: var(--main); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/341988909363757057" target="_blank"><i class="fab fa-discord" style="color: var(--main); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="assets/images/team/joshua.gif" class="owner" alt="">
+                <h2 style="color: var(--main)">Spyro (Joshua)</h2>
+                <p style="color: var(--main)">Owner</p>
+                <a href="https://github.com/DeltaSLM" target="_blank"><i class="fab fa-github" style="color: var(--main); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/JamesStrEVs" target="_blank"><i class="fab fa-twitter" style="color: var(--main); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/839237573595365406" target="_blank"><i class="fab fa-discord" style="color: var(--main); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="whiteground">
+    <iframe src="https://discord.com/widget?id=715969701771083817&theme=dark" width="100%" height="500" allowtransparency="true" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 </div>
 
 <script src="assets/js/navbar.js"></script>
