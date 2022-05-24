41e5e0ddcac2314b2bc84d9c543462df28a6dd98
Hilex23 <89878821+Hilex23@users.noreply.github.com>
2022-05-25 00:50:08 +0200

    pingernos page

---
+++ pingernos.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/head.html' ;?>
+
+<?php include 'templates/navbar.html' ;?>
+
+<body>
+
+    <div class="home container text-center">
+        <h1 class="main-title">Pingernos</h1>
+        <img src="/assets/images/projects/pingernos.png" class="pingernos-logo" alt="">
+        <p class="main-text">
+            Pingernos is an unofficial Discord bot made by <a href="https://zoniq.tk" target="_blank">Joshua</a>, and contributed to by <a href="https://hilex23.tk" target="_blank">Hilex23</a> and Toothy that lets you grab the status of any Aternos server.
+            <br/>
+            The bot's prefix is a mention. To get a list of the bot's commands you can use <code><@889197952994791434> help</code>
+            <br/>
+            The API we use is not affiliated with Aternos, Minecraft or any server host. This bot is also not affiliated with Aternos.
+            <br/>
+            <br/>
+            Main commands:
+        </p>
+    </div>
+    <div class="container">
+        <div class="commands">
+            <p class="dotted subtext-2">
+                <code><@889197952994791434> status [Aternos name or IP]</code> lets you grab the status of the given Aternos server. Please note that custom domains (example.com, example.org, etc.) are not supported, the server IP must end in '.aternos.me' but you are not obligated to add it while using the bot (example = example.aternos.me).
+            </p>
+                <br/>
+                <br/>
+            <p class="dotted subtext-3">
+                <code><@889197952994791434> info</code> displays all kind of info regarding Pingernos, including the invite link, the GitHub page, and more.
+            </p>
+                <br/>
+                <br/>
+            <p class="dotted subtext-3">
+                <code><@889197952994791434> info</code> displays our privacy policy (https://psychops.tk/privacy.php)
+            </p>
+        </div>
+    </div>
+    <div class="container text-center">
+        <div class="txt-btn-div">
+            <a href="https://discord.gg/QMhvHuGPMx" class="txt-btn double-btn">Discord Server</a>
+            <a href="https://discord.com/api/oauth2/authorize?client_id=889197952994791434&permissions=274878286848&scope=bot" target="_blank" class="txt-btn double-btn">Invite It!</a>
+        </div>
+    </div>
+    <br/>
+    <br/>
+
+    <script src="assets/js/navbar.js"></script>
+
+</body>
+
+</html>
