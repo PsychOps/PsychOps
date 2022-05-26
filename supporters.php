ce5b23e05554b42ead38396fbc190820520c6ffd
joshua <joshuaslui0203@gmail.com>
2022-05-26 19:04:25 +0200

    Automatically fetch avatars from discord

--- supporters.php
+++ supporters.php
@@ -5,6 +5,8 @@
 
 <?php include 'templates/navbar.html' ;?>
 
+<?php include 'assets/php/userav.php' ;?>
+
 <body>
 
     <div class="team container text-center">
@@ -12,20 +14,20 @@
         <p class="main-text">We currently have 3 graphic designers that help us by creating amazing logos, banners, and much more.<br>Any and all logos created by them are owned by us, but honourably copyrighted to the responsible graphic designer.</p>
         <div class="team-row">
             <div class="member">
-                <img src="assets/images/supporters/amgelo.png" class="graphic-designer" alt="">
+                <img src="<?php getAv(235428738748121088); ?>" class="graphic-designer" alt="">
                 <h2>Amgelo</h2>
                 <p>Graphic Designer</p>
                 <a href="https://github.com/Amgelo563" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
                 <a href="https://discord.com/users/235428738748121088" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
             </div>
             <div class="member">
-                <img src="assets/images/team/somebody.png" class="graphic-designer" alt="">
+                <img src="<?php getAv(438049859262480386); ?>" class="graphic-designer" alt="">
                 <h2>Somebody</h2>
                 <p>Graphic Designer</p>
                 <a href="https://discord.com/users/438049859262480386" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
             </div>
             <div class="member">
-                <img src="assets/images/supporters/bigchez.png" class="graphic-designer" alt="">
+                <img src="<?php getAv(584672380085862407); ?>" class="graphic-designer" alt="">
                 <h2>Big Chez</h2>
                 <p>Graphic Designer</p>
                 <a href="https://twitter.com/carlwife42" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
