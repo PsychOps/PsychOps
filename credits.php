653b8744e9b85988e3eae73835563275204a5a83
Dobby-progam <dthweathertime@gmail.com>
2022-05-30 18:31:38 +0200

    updated

---
+++ credits.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/head.html' ;?>
+
+<?php include 'templates/navbar.html' ;?>
+
+<div class="team container text-center">
+<h1 class="main-title">Our credits:</h1>
+<p class="main-text"> we wand to say tanks to </p>
+<div class="team-row">
+<div class="member">
+                <img src="<?php getAv(235428738748121088); ?>" class="graphic-designer" alt="">
+                <h2>Amgelo</h2>
+                <p>For our old logo</p>
+                <a href="https://github.com/Amgelo563" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/235428738748121088" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="<?php getAv(438049859262480386); ?>" class="graphic-designer" alt="">
+                <h2>Somebody</h2>
+                <p>For our new logo</p>
+                <a href="https://discord.com/users/438049859262480386" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="<?php getAv(584672380085862407); ?>" class="graphic-designer" alt="">
+                <h2>Big Chez</h2>
+                <p>For the Val</p>
+                <a href="https://twitter.com/carlwife42" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="https://discord.com/users/584672380085862407" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+</div>
+
