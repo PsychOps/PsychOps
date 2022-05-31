9d8db70b65b1cd7d2b816023f3b1c1edd2cd11e5
Delta <joshuaslui0203@gmail.com>
2022-05-31 15:32:32 +0200

    fix things i didnt spot in pr

--- credits.php
+++ credits.php
@@ -5,11 +5,11 @@
 
 <?php include 'templates/navbar.html' ;?>
 
-<?php include '/assets/php/userav.php'; ?>
+<?php include 'assets/php/userav.php' ;?>
 
 <div class="team container text-center">
 <h1 class="main-title">Our credits:</h1>
-<p class="main-text"> we wand to say tanks to </p>
+<p class="main-text"> we want to say thanks to</p>
 <div class="team-row">
 <div class="member">
                 <img src="<?php getAv(235428738748121088); ?>" class="graphic-designer" alt="">
@@ -27,7 +27,7 @@
             <div class="member">
                 <img src="<?php getAv(584672380085862407); ?>" class="graphic-designer" alt="">
                 <h2>Big Chez</h2>
-                <p>For the Val</p>
+                <p>For Valery</p>
                 <a href="https://twitter.com/carlwife42" target="_blank"><i class="fab fa-twitter" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
                 <a href="https://discord.com/users/584672380085862407" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
             </div>
