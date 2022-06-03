63ae17583e9ef5804fe9617ff423692909152c2a
Hilex23 <89878821+Hilex23@users.noreply.github.com>
2022-06-04 00:51:31 +0200

    unimporting the imported imports

--- jobs/developer.php
+++ jobs/developer.php
@@ -1,9 +1,108 @@
 <!DOCTYPE HTML>
 <html lang="en">
 
-<?php include 'templates/head.html';?>
+<head>
+    <!--Page Info-->
+    <title>PsychOps Studios</title>
+    <meta name="description" content="a producer of completely free, open source user-friendly products">
+    <meta name="keywords" content="psychops, psychops studios, psychops development, modraxis, varumox, pingernos, serverpinger, manuel maiani, joshua slui">
+    <meta name="author" content="Manuel Maiani, Joshua Slui">
+    <link rel="icon" href="/assets/images/psychops/logo.png">
+  
+    <!--Page OGP-->
+    <meta property="og:title" content="PsychOps Studios" />
+    <meta property="og:description" content="a producer of completely free, open source user-friendly products" />
+    <meta property="og:site_name" content="PsychOps" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://psychops.eu/" />
+    <meta property="og:image" content="https://psychops.eu/assets/images/psychops/logo.png" />
+    <meta property="og:locale" content="en_US" />
+  
+    <!--Bootstrap imports-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    
+    <!--JQuery Imports-->
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+    
+    <!--Fontawesome icons-->
+    <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
+    
+    <!--Boxicons-->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+  
+    <!--Personal (s)css imports-->
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/navbar.css">
+    
+    <!--Viewport-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 
-<?php include 'templates/navbar.html';?>
+<header>
+    <nav class="navbar">
+        <div class="branding">
+            <h2><a href="index.php" class="branding-logo">PSYCHOPS STUDIOS</a></h2>
+        </div>
+        <label for="input-hamburger" class="hamburger "></label>
+        <input type="checkbox" id="input-hamburger" hidden>
+        <ul class="menu">
+            <li><a href="index.php" class="menu-link">HOME</a></li>
+            <li><a href="/privacy.php" class="menu-link">PRIVACY</a></li>
+            <li class="has-dropdown">
+                <a href="#" class="menu-link">ORGANIZATION
+                    <span class="arrow"></span>
+                </a>
+                <ul class="submenu">
+                    <li><a href="about.php" class="menu-link">About Us</a></li>
+                    <li class="has-dropdown">
+                        <a href="#" class="menu-link">Our Team
+                            <span class="arrow"></span>
+                        </a>
+                        <ul class="submenu">
+                            <li><a href="/team.php" class="menu-link">PsychOps Developers</a></li>
+                            <li><a href="/supporters.php" class="menu-link">PsychOps Supporters</a></li>
+                        </ul>
+                    </li>
+                    <li class="has-dropdown">
+                        <a href="#" class="menu-link">Partners
+                            <span class="arrow"></span>
+                        </a>
+                        <ul class="submenu">
+                            <li><a href="/mcgolden.php" class="menu-link">Minecraft Golden</a></li>
+                            <li><a href="/tpk.php" class="menu-link">The Patris Kingdom</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="/jobs.php" class="menu-link">Positions</a></li>
+                    <li><a href="/credits.php" class="menu-link">Credits</a></li>
+                </ul>
+                
+            </li>
+            <li class="has-dropdown">
+                <a href="#" class="menu-link">PROJECTS
+                    <span class="arrow"></span>
+                </a>
+                <ul class="submenu">
+                    <li><a href="/pingernos.php" class="menu-link">Pingernos</a></li>
+                </ul>
+            </li>
+            <li class="has-dropdown">
+                <a href="#" class="menu-link">CONTACT US
+                    <span class="arrow"></span>
+                </a>
+                <ul class="submenu">
+                    <li><a href="https://discord.gg/QMhvHuGPMx" target="_blank" class="menu-link">Discord</a></li>
+                    <li><a href="https://twitter.com/PsychOpsOrg" class="menu-link">Twitter</a></li>
+                    <li><a href="https://reddit.com/r/PsychOps">Reddit</a></li>
+                    <li><a href="mailto:support@psychops.eu">Support e-mail</a></li>
+                    <li><a href="mailto:contact@psychops.eu">Contact e-mail</a></li>
+                    <!--<li><a href="contact.php" class="menu-link">Other</a></li>-->
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
 
 <body>
 
