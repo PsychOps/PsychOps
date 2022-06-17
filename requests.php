c0a40e47ab6dc2b2e3f2dd9295f669210d7ad216
Delta <joshuaslui0203@gmail.com>
2022-06-18 01:34:44 +0200

    Add project requests

---
+++ requests.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php include 'templates/head.html' ;?>
+
+<?php include 'templates/navbar.html' ;?>
+
+<body>
+
+    <div class="team container text-center">
+        <h1 class="main-title">Project Requests</h1>
+        <p class="main-text">We offer the possibility to <b>freely</b> request us to develop projects for you. This mainly includes personal websites, but can also include professional websites or other types of projects such as discord bots. Of course, because it's completely free (without any hidden catch), we can't take all requests coming our way and must carefully review each one. We don't have specific criteria, but please feel free to always contact us with your request at <a href="mailto:contact@psychops.eu" target="_blank">contact@psychops.eu</p>
+        <div class="team-row">
+            <div class="member">
+                <img src="assets/images/psychops/logo.png" class="owner" alt="">
+                <h2>MG</h2>
+                <p>W.I.P. request</p>
+                <a href="https://github.com/PsychOps/MGweb" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+            <div class="member">
+                <img src="assets/images/psychops/logo.png" class="owner" alt="">
+                <h2>TPK</h2>
+                <p>W.I.P. request</p>
+                <a href="https://github.com/PsychOps/TPKweb" target="_blank"><i class="fab fa-github" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-discord" style="color: var(--txt); font-size: 30px; margin-right: 10px; margin-left: 10px;" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <script src="assets/js/navbar.js"></script>
+
+</body>
+
+</html>
