12a50bd770af5f5dc770a60f63d2facbc4da608e
DeltaSLM <joshuaslui0203@gmail.com>
2022-10-07 21:56:41 +0200

    Beep, Boop. Goodbye PsychOps. You are relieved from duty.

--- assets/php/userav.php
+++ assets/php/userav.php
@@ -28,8 +28,40 @@ function getAv($id)
     $a .= "?size=2048";
     echo $a;
 }
+
+function getName($id)
+{
+
+    $bot_token = '';
+
+    include 'environment.php';
+    $json_options = [
+        "http" => [
+            "method" => "GET",
+            "header" => "Authorization: Bot $bot_token"
+        ]
+    ];
+
+    $url = 'https://discordapp.com/api/users/';
+    $url .= $id;
+
+    $json_context = stream_context_create($json_options);
+
+    $json_get = file_get_contents($url, false, $json_context);
+
+    $json_decode = json_decode($json_get, true);
+
+    $a = $json_decode['username'];
+    $a .= '#';
+    $a .= $json_decode['discriminator'];
+    echo $a;
+
+    //echo $a;
+}
 ?>
 
 <!-- EXAMPLE OF HOW TO USE THIS
 <img src="<?php getAv(511590780834545664) ?>" alt="" height="500px">
+
+<p><?php getName(511590780834545664) ?></p>
 -->
--- index.php
+++ index.php
@@ -3,20 +3,266 @@
 
 <?php include 'templates/head.html' ;?>
 
-<?php include 'templates/navbar.html' ;?>
+<?php include 'assets/php/userav.php'?>
+
+<style>
+    .imgspecial {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 10%;
+    }
+
+    hr {
+        border-top: 10px solid darkblue;
+        border-radius: 2px;
+    }
+    button {
+        font-weight: bold;
+        color: var(--executive) !important;
+        text-align: center !important;
+    }
+    h1, h2 {
+        color: var(--social)
+    }
+</style>
+
 
 <body>
 
-    <div class="home container text-center">
-        <h1 class="main-title">PsychOps Studios</h1>
-        <img src="/assets/images/psychops/logo.png" class="home-logo" alt="">
-        <p class="main-text">A producer of completely free, open source user-friendly products</p>
-        <div class="txt-btn-div">
-            <a href="about.php" class="txt-btn">About Us</a>
+<div class="container" style="text-align: center; padding-bottom: 10px">
+    <img src="assets/images/psychops/logo.png" alt="" style="padding-top: 20%;" class="imgspecial">
+    <h1>Thank you for your support.</h1>
+    PsychOps Studios was created with the purpose of creating new fully free and open source products for everyone to use. But unfortunately, that journey ends for us here.
+    <br>
+    <a href="#pingernos">What about Pingernos?</a> | <a href="#q&a">Questions & Answers</a> | <a href="#ulafo">Ulafo</a>
+    <hr>
+    <h2>Our gratitude goes out to</h2>
+    <div class="justify-content-center" style="display: flex; flex-direction: row; flex-wrap: wrap; gap: 1rem">
+        <div class="d-flex" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--owner)">
+                <div class="top-container">
+                    <img src="<?php getAv(839237573595365406) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(839237573595365406) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Owner & Founder</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--owner)">
+                <div class="top-container">
+                    <img src="<?php getAv(370258256775479307) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(370258256775479307) ?></h5>
+                        <p class="mail" style="color: var(--main);text-align: left;">Owner</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--owner)">
+                <div class="top-container">
+                    <img src="<?php getAv(912852147392102421) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(912852147392102421) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Founder</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--executive)">
+                <div class="top-container">
+                    <img src="<?php getAv(438049859262480386) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(438049859262480386) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Project Manager</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--executive)">
+                <div class="top-container">
+                    <img src="<?php getAv(227464680237432833) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(227464680237432833) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Financial Advisor</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--team)">
+                <div class="top-container">
+                    <img src="<?php getAv(345457928972533773) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(345457928972533773) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--team)">
+                <div class="top-container">
+                    <img src="<?php getAv(104933285506908160) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(104933285506908160) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--team)">
+                <div class="top-container">
+                    <img src="<?php getAv(820255805257023498) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(820255805257023498) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--team)">
+                <div class="top-container">
+                    <img src="<?php getAv(282825580141871104) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(282825580141871104) ?></h5>
+                        <p class="mail" style="color: var(--main);text-align: left;">Developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--team)">
+                <div class="top-container">
+                    <img src="<?php getAv(341988909363757057) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(341988909363757057) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--social)">
+                <div class="top-container">
+                    <img src="<?php getAv(360782146899804161) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(360782146899804161) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Media management</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--graphic)">
+                <div class="top-container">
+                    <img src="<?php getAv(235428738748121088) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(235428738748121088) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Graphic Designer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mobile-padding" style="flex-wrap: wrap">
+            <div class="card" style="background-color: var(--graphic)">
+                <div class="top-container">
+                    <img src="<?php getAv(584672380085862407) ?>" class="img-fluid profile-image" width="70" alt="">
+                    <div class="ml-3">
+                        <h5 class="name" style="color: var(--main)"><?php getName(584672380085862407) ?></h5>
+                        <p class="mail" style="color: var(--main); text-align: left">Graphic Designer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        These, and many other people have together been a meaningful part of our community supporting us in our goals and actively participating in PsychOps. Their services are appreciated and will be remembered thankfully by us. We would also like to thank Etile for providing PsychOps Studios with infrastructure to host on when we first started.
+    </div>
+    <hr>
+    <div class="container" id="pingernos">
+        <h2>But what about Pingernos?</h2>
+        Pingernos will remain online and will continue to be actively maintained by <?php getName(839237573595365406); ?> and <?php getName(370258256775479307); ?>. For questions, you can still join the PsychOps discord server. After PsychOps gets dismantled completely on 01/06/2023, Pingernos will continue under its own name as a stand-alone product.
+    </div>
+    <hr>
+    <div class="container" id="q&a">
+        <h2 id="q&a">Questions & Answers</h2>
+        <div class="accordion accordion-flush" id="accordionAnswers1">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        What will happen to the discord?
+                    </button>
+                </h2>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body" style="text-align: left">The discord server will remain open but may undergo changes to fit in more as a community server. It will continue to serve as a support hub for Pingernos, and development support and/or snippet sharing.</div>
+                </div>
+            </div>
+        </div>
+        <div class="accordion accordion-flush" id="accordionAnswers2">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        Why are you shutting down?
+                    </button>
+                </h2>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body" style="text-align: left">Unfortunately, we were short on developers with enough experience to make reality what we had in mind. PsychOps Studios has been an amazing journey for us all, but it has become impossible to continue it.</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="accordion accordion-flush" id="accordionAnswers3">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        Will PsychOps ever be revived?
+                    </button>
+                </h2>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body" style="text-align: left">To be completely straight with you, no. PsychOps will never be revived in its current form or branding. If we ever decide that a new oranization is in its place, we will make it a fresh start. For now, we recommend checking out <a href="https://discord.gg/Y7XxK4St9D" target="_blank">Ulafo</a>, a company by our friends.</div>
+                </div>
+            </div>
         </div>
     </div>
+    <hr>
+    <div class="container" id="ulafo">
+        <img src="assets/images/Ulafo.png" width="50%" alt="">
+        <p>Ulafo is a digital creative agency specializing in graphic design and website development.</p>
 
-    <script src="assets/js/navbar.js"></script>
+        <p>Their dedicated team creates a wide range of graphics, as well as develops websites. They seek to deliver the best experience for their clients by helping them save their time and money.</p>
+        <a class="btn btn-primary" href="https://discord.gg/Y7XxK4St9D">Join Ulafo</a>
+        <br>
+        <a href="https://twitter.com/UlafoAgency" style="color: var(--main-black); padding: 10px; text-decoration: none">
+            <i class="fab fa-twitter fa-2x"></i>
+        </a>
+        <a href="https://www.instagram.com/ulafoagency/" style="color: var(--main-black); padding: 10px; text-decoration: none">
+            <i class="fab fa-instagram fa-2x"></i>
+        </a>
+        <a href="https://www.tiktok.com/@ulafo" style="color: var(--main-black); padding: 10px; text-decoration: none">
+            <i class="fab fa-tiktok fa-2x"></i>
+        </a>
+    </div>
+    <hr>
+    Beep, Boop, PsychOps Studios out.
+</div>
 
 </body>
 
